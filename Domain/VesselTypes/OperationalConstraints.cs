e9bde306ffd994e441b26d6b19512ff6f0e7489d
adampawelka <adampawelka04@gmail.com>
2025-10-16 20:28:49 +0200

    mend
    feat: Add VesselTypeId and OperationalConstraints domain models

--- Domain/VesselTypes/OperationalConstraints.cs
+++ Domain/VesselTypes/OperationalConstraints.cs
@@ -2,25 +2,26 @@ using System;
 using DDDSample1.Domain.Shared;
 
 namespace DDDSample1.Domain.VesselTypes {
-    public class OperationalConstraints : ValueObject {
-    public int MaxRows { get; private set; }
-    public int MaxBays { get; private set; }
-    public int MaxTiers { get; private set; }
+    public class OperationalConstraints : IValueObject {
+        public int MaxRows { get; private set; }
+        public int MaxBays { get; private set; }
+        public int MaxTiers { get; private set; }
 
-    private OperationalConstraints() {
+        private OperationalConstraints() {
 
-    }
-    public OperationalConstraints(int maxRows, int maxBays, int maxTiers) {
-        if (maxRows <= 0)
-            twrow new BusinessRuleValidationException("Maximum rows must be positive.");
-        if (maxBays <= 0)
-            throw new BusinessRuleValidationException("Maximum bays must be positive.");
-        if (maxTiers <= 0)
-            throw new BusinessRuleValidationException("Maximum tiers must be positive.");
+        }
+    
+        public OperationalConstraints(int maxRows, int maxBays, int maxTiers) {
+            if (maxRows <= 0)
+                throw new BusinessRuleValidationException("Maximum rows must be positive.");
+            if (maxBays <= 0)
+                throw new BusinessRuleValidationException("Maximum bays must be positive.");
+            if (maxTiers <= 0)
+                throw new BusinessRuleValidationException("Maximum tiers must be positive.");
 
-        this.MaxRows = maxRows;
-        this.MaxBays = maxBays;
-        this.MaxTiers = maxTiers;
+            this.MaxRows = maxRows;
+            this.MaxBays = maxBays;
+            this.MaxTiers = maxTiers;
         }
     }
 }
