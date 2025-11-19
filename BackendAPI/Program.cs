5ffd0e6653fdb7e68c2cea5d30c7491121d5fac0
adampawelka <adampawelka04@gmail.com>
2025-11-19 18:42:15 +0000

    feat(backend): Add auto migration to Program.cs

--- BackendAPI/Program.cs
+++ BackendAPI/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection; 
+using Microsoft.EntityFrameworkCore;           
+using System;
 
 namespace Backend
 {
@@ -7,7 +10,24 @@ namespace Backend
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<Backend.Infrastructure.DDDSample1DbContext>();
+
+                    context.Database.Migrate();
+                    Console.WriteLine("Database Migrated Successfully.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Database Migration Failed: {ex.Message}");
+                }
+            }
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
